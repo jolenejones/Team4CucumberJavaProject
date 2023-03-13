e19bf0d517b612e385cd7d59dad50c39efc2d145
Jolene Jones <jolenejoba@gmail.com>
2023-03-13 14:23:13 -0400

    "init git repo set up"

---
+++ src/test/java/uiStepDefinitions/SearchStepDefinitions.java
@@ -0,0 +1,40 @@
+package uiStepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SearchStepDefinitions {
+
+	@Given("browser is open")
+	public void browser_is_open() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@And("user is on youtube search page")
+	public void user_is_on_youtube_search_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user enters a text in search box")
+	public void user_enters_a_text_in_search_box() {
+	    driver.find
+	}
+
+	@When("hits enter")
+	public void hits_enter() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user is navigated to search results")
+	public void user_is_navigated_to_search_results() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	}
+
---
+++ src/test/java/utilities/DriverManager.java
@@ -0,0 +1,74 @@
+package utilities;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverManager {
+
+	private static final String DRIVER_MAC = "src/test/resources/drivers/chromedriver_mac";
+	private static final String DRIVER_MAC_M1 = "src/test/resources/drivers/chromedriver_mac_m1";
+	private static final String DRIVER_WINDOWS = "src/test/resources/drivers/chromedriver_windows.exe";
+
+	private static WebDriver driver;
+	private static String customSessionId;
+
+	private DriverManager() {
+	};
+
+	public static WebDriver getInstance() {
+		try {
+			if (driver == null) {
+				System.setProperty("webdriver.chrome.driver", getDriverPath());
+				ChromeOptions cOption = new ChromeOptions();
+				cOption.addArguments("--remote-allow-origins=*");
+				driver = new ChromeDriver(cOption);
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+			}
+		} catch (Exception e) {
+			assertTrue(false, e.getMessage());
+		}
+		return driver;
+	}
+
+	public static String getSessionId() {
+		if (customSessionId == null) {
+			customSessionId = Util.get6DigitCode() + "";
+		}
+		return customSessionId;
+	}
+
+	public static void tearDown() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+		if (customSessionId != null) {
+			customSessionId = null;
+		}
+	}
+
+	private static String getDriverPath() throws Exception {
+		Properties sProps = System.getProperties();
+		String os = sProps.getProperty("os.name").toLowerCase();
+		String archType = sProps.getProperty("os.arch").toLowerCase();
+
+		if (os.startsWith("mac")) {
+			if (archType.startsWith("aarch64")) {
+				return DRIVER_MAC_M1;
+			} else {
+				return DRIVER_MAC;
+			}
+		} else if (os.startsWith("windows")) {
+			return DRIVER_WINDOWS;
+		} else {
+			throw new Exception("No driver available for your system");
+		}
+	}
+
+}
